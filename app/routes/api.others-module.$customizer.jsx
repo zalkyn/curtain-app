e6599777501f2df6d920c1d0a954078a16adb1cd
Ulysse <ulysse@shieldprime.app>
2025-07-20 23:06:32 +0600

    update others module api on july 20, v4

--- app/routes/api.others-module.$customizer.jsx
+++ app/routes/api.others-module.$customizer.jsx
@@ -68,7 +68,7 @@ export const loader = async ({ request, params }) => {
     const trim = customizer?.trims[0] || null;
     const roomLabel = customizer?.roomLabel[0] || null;
     const trackSize = customizer?.trackSize?.[0] || null;
-    const liftType = customizer?.LiftType?.[0] || null;
+    const liftType = customizer?.liftType?.[0] || null;
 
     // wait 20 seconds 
     // await new Promise(resolve => setTimeout(resolve, 20000));
